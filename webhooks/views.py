0200cb8068f50efdcd20ec0e94d8cebb90b8bc7f
Julio Gazoli <julio_gazoli@hotmail.com>
2024-10-07 16:10:28 -0300

    start project and start app webhooks

---
+++ webhooks/views.py
@@ -0,0 +1,12 @@
+from rest_framework import views, response, status
+
+
+class WebhookOrderView(views.APIView):
+
+    def post(self, request):
+        data = request.data
+        return response.Response(
+            data=data,
+            status=status.HTTP_200_OK,
+        )
+
